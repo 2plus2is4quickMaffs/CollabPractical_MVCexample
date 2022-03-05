43ccd41ecb2eeb90e8c2b3ca5cde745ea7fda742
haarishKhan007 <100934179+haarishKhan007@users.noreply.github.com>
2022-03-05 16:16:02 +0400

    change title of controller 2 by adding extra parameter

--- JavaApplication6/src/mvcexample/Main.java
+++ JavaApplication6/src/mvcexample/Main.java
@@ -7,17 +7,15 @@ package mvcexample;
 // Launcher for application
 
 public class Main {
-   
-    public static void main(String args[]) {
-     
-        Model model = new Model();               // Create shared database
-        Controller1 c1 = new Controller1(model); // Create first controller
-        Controller2 c2 = new Controller2(model,"Controller C2",40,400); // Create second controller
-         Controller2 c3 = new Controller2(model,"Controller C3",50,400); // Create third controller
-          Controller2 c4 = new Controller2(model,"Controller C4",60,400); // Create fourth controller
-         
-        
-    } // main
-  
-} // Main  
 
+	public static void main(String args[]) {
+
+		Model model = new Model(); // Create shared database
+		Controller1 c1 = new Controller1(model); // Create first controller
+		Controller2 c2 = new Controller2(model, "Controller C2", 40, 400); // Create second controller
+		Controller2 c3 = new Controller2(model, "Controller C3", 50, 400); // Create third controller
+		Controller2 c4 = new Controller2(model, "Controller C4", 60, 400); // Create fourth controller
+
+	} // main
+
+} // Main
